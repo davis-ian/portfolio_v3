67a731d1696339a2199d1f7ce6495a868a9931c4
Ian Davis <iandavis6481@gmail.com>
2025-05-31 09:55:56 -0500

    mobile projects layout

--- components/ProjectBar.vue
+++ components/ProjectBar.vue
@@ -1,20 +1,27 @@
 <template>
-    <div class="flex flex-col group" @mouseenter="emit('hover')" @mouseleave="emit('leave')">
+    <div class="flex flex-col group relative">
         <div class="border-solid flex-grow-1">
             <div class="relative overflow-hidden h-full">
                 <img
-                    :src="props.project.imageSrc"
+                    :src="project.imageSrc"
                     alt="Project Preview"
-                    class="transition duration-300 h-50 w-full md:h-full ease-in-out filter object-cover object-center brightness-75 grayscale"
-                    :class="{ 'brightness-100 grayscale-0': props.isActive }"
+                    class="transition duration-300 h-60 w-full md:h-full ease-in-out filter object-cover object-center brightness-100 grayscale-0 md:brightness-75 md:grayscale md:group-hover:brightness-100 md:group-hover:grayscale-0"
                 />
             </div>
         </div>
+
         <div
-            class="bg-black text-white text-2xl overflow-hidden h-0 transition-all duration-500 ease-in-out flex"
-            :class="props.isActive ? 'h-20' : 'h-0'"
+            class="bg-black hidden md:flex text-white text-2xl overflow-hidden transition-all duration-500 ease-in-out w-full h-20 md:h-0 md:group-hover:h-20"
         >
             <span class="m-4">
+                {{ project.name }}
+            </span>
+        </div>
+
+        <div
+            class="bg-black absolute bottom-0 left-0 flex w-fit md:hidden text-white text-xl overflow-hidden transition-all duration-500 ease-in-out p-2"
+        >
+            <span>
                 {{ project.name }}
             </span>
         </div>
@@ -24,10 +31,5 @@
 <script setup>
 const props = defineProps({
     project: Object,
-    isActive: Boolean,
 });
-
-const emit = defineEmits(['hover', 'leave']);
 </script>
-
-<style lang="scss" scoped></style>
