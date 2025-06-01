a18838bc3d97eabf1b7e2de3ac0022540d34d9df
Ian Davis <iandavis6481@gmail.com>
2025-06-01 10:49:41 -0500

    fade page transition

---
+++ app.vue
@@ -0,0 +1,25 @@
+<template>
+    <NuxtLayout>
+        <NuxtPage :transition="pageTransition" />
+    </NuxtLayout>
+</template>
+
+<script setup>
+const pageTransition = {
+    name: 'page-fade-slide',
+    mode: 'out-in',
+};
+</script>
+
+<style>
+.page-fade-slide-enter-active,
+.page-fade-slide-leave-active {
+    transition: opacity 0.3s ease, transform 0.3s ease;
+}
+
+.page-fade-slide-enter-from,
+.page-fade-slide-leave-to {
+    opacity: 0;
+    transform: translateY(6px);
+}
+</style>
